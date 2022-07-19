efbce063c2364f7bc944a6cfcafdb601df804f41
Chandrakishor Tiwari <chandrkishor@mindrops.com>
2022-07-19 08:55:02 +0530

    redux toolkit thunk added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { uiActions } from "./components/actionSlices/UI_Slice";
 import Notification from "./components/UI/Notification";
+import { sendCardData } from "./components/actionSlices/CardSlice";
 
 let isInitial = true;
 
@@ -15,44 +15,11 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const sendCardData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch("https://reduxapicallingtest-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json", {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-      const responseData = await response.json();
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Send cart data successfully",
-        })
-      );
-    };
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCardData().catch((error) =>
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "error!",
-          message: "Sending cart data failed",
-        })
-      )
-    );
+    dispatch(sendCardData(cart));
   }, [cart, dispatch]);
   return (
     <>
